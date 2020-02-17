cfa908eb2a8efd543a591fe4f02f0d0887cc45af
Luke W. Johnston <lwjohnst@gmail.com>
2020-02-17 22:38:35 +0100

    usethis ui functions

---
+++ R/lwj-package.R
@@ -0,0 +1,9 @@
+#' @keywords internal
+#' @importFrom usethis ui_todo ui_done ui_code_block
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
